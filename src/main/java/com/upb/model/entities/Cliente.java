3ecab04bbca6ed537e111d87e0e7e5d48ad23d20
Dcanosu <dcanosu@gmail.com>
2025-04-29 17:59:24 -0500

    Creacion de nuevas entidades

--- src/main/java/com/upb/model/entities/Cliente.java
+++ src/main/java/com/upb/model/entities/Cliente.java
@@ -1,15 +1,19 @@
 package com.upb.model.entities;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Setter
 @Getter
-@Builer
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "clientes")
--- src/main/java/com/upb/model/entities/ServicioMoto.java
+++ src/main/java/com/upb/model/entities/ServicioMoto.java
@@ -1,4 +1,6 @@
 package com.upb.model.entities;
+import java.sql.Date;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -11,19 +13,26 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+
 @Entity
 @Setter
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "motos")
-public class Moto {
+@Table(name = "servicio_moto")
+public class ServicioMoto {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    String marca;
-    String modelo;
-    String placa;
-    @Column(name = "cliente_id")
-    Long fk_cliente_id;
+    Long moto_id;
+    Long servicio_id;
+    Long cargo_id;
+    Date fecha_servicio;
+    String comentarios;
+    @Column(name = "moto_id")
+    Long fk_moto_id;
+    @Column(name = "servicio_id")
+    Long fk_servicio_id;
+    @Column(name = "cargo_id")
+    Long fk_cargo_id;
 }
